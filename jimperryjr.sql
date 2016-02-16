9cd377d44483b7ce262880969a855644f2317541
jim perry jr - m.a.c.s <jperry11@my.westga.edu>
2016-02-15 21:43:43 -0500

    # defining foreign and primary keys

--- jimperryjr.sql
+++ jimperryjr.sql
@@ -13,16 +13,19 @@ USE
 
 CREATE TABLE album_title(
 	id_album		int			not null	primary key		auto_increment,
-	id_artist		int			not null	foreign key		auto_increment,
+	id_artist		int			not null	foreign key,
 	album_name		varchar(60)	not null,
 	album_date		date		not null
+	
+	primary key (id_album, track_number)
+	foreign key (id_artist) references artist(id_artist)
 );
 
 
 /*create tables*/
 
 CREATE TABLE artist(
-	id_album	int			not null	primary key		auto_increment,
+	id_artist	int			not null	primary key		auto_increment,
 	artist_name	varchar(60)	not null
 );
 
@@ -30,10 +33,11 @@ CREATE TABLE artist(
 /*create tables*/
 
 CREATE TABLE album_content (
-	id_album
-	track_name
+	id_album		int			not null,
+	track_name		varchar(60)	not null,
 	track_time		time		not null,	
 	track_artist	varchar(60)	not null,
 	track_number	int 		not null,
+	
 	primary key (id_album, track_number)
 );
