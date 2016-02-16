c035a4af8826fcd686c9ed572c51bf046e67751c
jim perry jr - m.a.c.s <jperry11@my.westga.edu>
2016-02-15 20:38:27 -0500

    initial commit

---
+++ jimperryjr.sql
@@ -0,0 +1,29 @@
+/*create database my_guitar_shop3;*/
+create database if not exists jim_music_albums;
+drop database if exists jim_music_albums;
+use jim_music_albums;
+
+
+DROP DATABASE IF EXISTS
+  jim_music_albums;
+CREATE DATABASE IF NOT EXISTS jim_music_albums;
+USE
+  jim_music_albums;
+
+
+/*create tables*/
+CREATE TABLE customers(
+
+);
+
+
+/*create tables*/
+CREATE TABLE customers(
+
+);
+
+
+/*create tables*/
+CREATE TABLE customers(
+ 
+);
